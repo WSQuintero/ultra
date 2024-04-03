b7f70e89b1d27887b42188b5a3da9514bd505083
Christian Garcerant <christian.garcerant@uac.edu.co>
2024-04-03 16:27:44 -0400

    Shared landing button created and everything imported

---
+++ src/components/landing/GoldButton.jsx
@@ -0,0 +1,15 @@
+import { Button, styled } from "@mui/material";
+
+export const GoldButton = styled(Button)({
+  // backgroundColor: 'linear-gradient(140.75deg, #8F5F25 11.94%, #DBBB6F 61.03%, #8F5F25 113.42%)',
+  background: 'linear-gradient(140.75deg, #8F5F25 11.94%, #DBBB6F 61.03%, #8F5F25 113.42%);',
+  '&:hover': {
+    opacity: '0.9',
+    boxShadow: 'none',
+  },
+  '&:active': {
+    boxShadow: 'none',
+    background: '#0062cc',
+    borderColor: '#005cbf',
+  },
+});
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,15 @@
-import HeaderLanding from "../components/landing/HeaderLanding"
+import MainSection from "../components/landing/MainSection"
+import LogosSection from "../components/landing/LogosSection"
+import Navbar from "../components/landing/NavBar"
+import OurMissionSection from "../components/landing/OurMissionSection"
 
 function Landing() {
   return (
-    <div>
-      hola
-      <HeaderLanding />
+    <div style={{background: "rgba(16, 17, 21, 1)"}} >
+      <Navbar />
+      <MainSection/>
+      <LogosSection/>
+      <OurMissionSection/>
     </div>
   )
 }
