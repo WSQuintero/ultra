a6beb3b92d19e745d160083e57e6a4c5982cba59
Christian Garcerant <christian.garcerant@uac.edu.co>
2024-04-10 20:07:52 -0500

    CREATE responsive styling for KeepUpdated section

--- src/components/landing/KeepUpdatedSection.jsx
+++ src/components/landing/KeepUpdatedSection.jsx
@@ -1,11 +1,13 @@
 import { Box, InputAdornment, OutlinedInput, Typography } from '@mui/material';
 import { GoldButton } from './GoldButton';
-import { styled } from '@mui/system';
+import { styled, useMediaQuery } from '@mui/system';
+import theme from '../../theme';
 
 const KeepUpdatedSection = () => {
+  const xxlMatches = useMediaQuery(() => theme.breakpoints.up('xxl'));
   return (
     <Box
-      id='contact'
+      id="contact"
       width="100%"
       height="100vh"
       display="flex"
@@ -17,62 +19,86 @@ const KeepUpdatedSection = () => {
       <img
         src="/Ultra_files/subscribe_bg.png"
         alt="Background"
-        style={{ position: 'absolute', width: '60%', height: '45%', zIndex: 0, borderRadius: '20px' }}
+        style={{
+          position: 'absolute',
+          width: xxlMatches ? '60%' : '90%',
+          height: xxlMatches ? '400px' : '300px',
+          zIndex: 0,
+          borderRadius: '20px',
+        }}
       />
 
-      <Box width="40%">
+      <Box sx={{ width: { xs: '80%', xxl: '40%' } }}>
         <Typography
           variant="h2"
           align="center"
           sx={{
             fontFamily: 'Bricolage Grotesque',
             fontWeight: 600,
-            fontSize: '48px',
+            fontSize: {
+              xs: '28px',
+              sm: '34px',
+              md: '38px',
+              lg: '44px',
+              xxl: '48px',
+            },
             lineHeight: '57.6px',
             color: 'white',
             position: 'relative',
-            zIndex: 1
+            zIndex: 1,
           }}
         >
           Stay Informed and never miss an crypteck update!
         </Typography>
       </Box>
-      <br/>
-      <br/>
+      <br />
+      <br />
 
       <CustomOutlinedInput
-      id="outlined-adornment-weight"
-      placeholder="Enter your email address"
-      endAdornment={
-        <InputAdornment position="end">
-          <GoldButton variant="contained" color="primary2">Subscribe now</GoldButton>
-        </InputAdornment>
-      }
-      aria-describedby="outlined-weight-helper-text"
-      inputProps={{
-        'aria-label': 'weight',
-      }}
-      style={{ zIndex: 1, backgroundColor: 'rgba(255, 255, 255, 0.18)', borderRadius: '15px', backdropFilter: 'blur(5px)', width: '35%' }}
-    />
+        id="outlined-adornment-weight"
+        placeholder="Enter your email address"
+        endAdornment={
+          <InputAdornment position="end">
+            <GoldButton variant="contained" color="primary2">
+              Subscribe now
+            </GoldButton>
+          </InputAdornment>
+        }
+        aria-describedby="outlined-weight-helper-text"
+        inputProps={{
+          'aria-label': 'weight',
+        }}
+        sx={{
+          zIndex: 1,
+          backgroundColor: 'rgba(255, 255, 255, 0.18)',
+          borderRadius: '15px',
+          backdropFilter: 'blur(5px)',
+          width: {
+            xs: '80%',
+            sm: '70%',
+            xxl: '35%'
+          },
+        }}
+      />
     </Box>
   );
 };
 const CustomOutlinedInput = styled(OutlinedInput)({
   '& .MuiOutlinedInput-input': {
     '&::placeholder': {
-      color: 'white', 
-      fontSize: '0.9rem', 
+      color: 'white',
+      fontSize: '0.9rem',
     },
   },
   '& .MuiOutlinedInput-root': {
     '&:hover fieldset': {
-      borderColor: 'white', 
+      borderColor: 'white',
     },
     '&.Mui-focused fieldset': {
-      borderColor: 'white', 
+      borderColor: 'white',
     },
     '&:focus-within fieldset': {
-      borderColor: 'white', 
+      borderColor: 'white',
     },
   },
 });
