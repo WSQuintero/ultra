0b1739e989784fe5f193c4ebfcd40ad207ffcfaa
Santiago Quintero <santiago@conexioncibernetica.com>
2024-04-11 05:18:15 -0500

    merge diferente branchs

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,19 +1,19 @@
 import MainSection from "../components/landing/MainSection"
 import TradingViewWidget from "../components/landing/LogosSection"
-import Navbar from "../components/landing/NavBar"
 import OurMissionSection from "../components/landing/OurMissionSection"
 import OurServicesSection from "../components/landing/OurServicesSection"
 import PricingSection from "../components/landing/PricingSection"
 import KeepUpdatedSection from "../components/landing/KeepUpdatedSection"
 import Footer from "../components/landing/Footer"
 import TelegramIcon from "../components/landing/TelegramFixIIcon"
+import Navbar from "../components/landing/NavBar"
 
 function Landing() {
   return (
-    <div style={{ background: 'rgba(16, 17, 21, 1)' }}>
+    <div style={{ background: "rgba(16, 17, 21, 1)" }}>
       <Navbar />
       <MainSection />
-      <div >
+      <div>
         <TradingViewWidget />
       </div>
       <OurMissionSection />
