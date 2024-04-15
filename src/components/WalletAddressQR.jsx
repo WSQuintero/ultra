dfd57a2052380e16ff1bf6547c14e443a0493e4c
Santiago Quintero <santiago@conexioncibernetica.com>
2024-04-15 12:23:09 -0500

    added square in QR

--- src/components/WalletAddressQR.jsx
+++ src/components/WalletAddressQR.jsx
@@ -2,7 +2,7 @@ import QRCode from "react-qr-code"
 
 function WalletAddressQR({ address }) {
   return (
-    <div>
+    <div style={{ border: "3px solid white", padding: "10px" }}>
       <QRCode value={address} size={250} />
     </div>
   )
